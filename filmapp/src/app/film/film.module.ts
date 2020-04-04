b0ecfbefc4d43842a2e1076dfdcfae9138830ce8
Audrey Behiels <audrey.behiels@student.hogent.be>
2020-04-04 18:46:40 +0200

    film-list component toegevoegd

--- filmapp/src/app/film/film.module.ts
+++ filmapp/src/app/film/film.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilmComponent } from './film/film.component';
 import {MaterialModule} from './../material/material.module';
+import { FilmListComponent } from './film-list/film-list.component';
 
 
 @NgModule({
-  declarations: [FilmComponent],
+  declarations: [FilmComponent, FilmListComponent],
   imports: [CommonModule, MaterialModule ],
   exports: [FilmComponent]
 })
