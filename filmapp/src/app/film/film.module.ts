f4418daf4e0349da45cabd229eb9ab53cdcd9e3d
Audrey Behiels <audrey.behiels@student.hogent.be>
2020-04-04 17:25:32 +0200

    default angular material thema toegevoegd, link om met iconen werken toegevoegd,

--- filmapp/src/app/film/film.module.ts
+++ filmapp/src/app/film/film.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilmComponent } from './film/film.component';
-
+import {MaterialModule} from './../material/material.module';
 
 
 @NgModule({
   declarations: [FilmComponent],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, MaterialModule ],
   exports: [FilmComponent]
 })
 export class FilmModule { }
